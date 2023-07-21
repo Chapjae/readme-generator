80c08e73f20313a1bf4c75a6b85010051364cf3a
Jay Liu <jayliu89@gmail.com>
2023-07-21 12:41:49 -0700

    initial commit

---
+++ Develop/index.js
@@ -0,0 +1,31 @@
+// TODO: Include packages needed for this application
+
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// Create a function that returns a license badge based on which license is passed in
+// add a packet that would import generate file
+// export *require* from a file const badmath = require(./badmath.js); 
+
+// If there is no license, return an empty string
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+//  create the readme
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    // ask the user questions - array
+    // need to save the answer to the questions somehow
+    // create content from the answers, potentially put them together into one long string (call generateMarkDown function?)
+    // insert object of answers
+    // call writeToFile
+    console.log("hello")
+}
+
+// Function call to initialize app
+let result = init(write);
+
