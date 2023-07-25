cfd3e9966eddccfe70233b4bcac7ea9ed13af33a
Jay Liu <jayliu89@gmail.com>
2023-07-24 17:29:57 -0700

    fixed badge links

--- DO NOT README.md
+++ DO NOT README.md
@@ -1,6 +1,6 @@
 #  #
   
-  ![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
   
   ## Description
   
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,15 +4,15 @@
 function renderLicenseBadge(license) {
   switch (license) {
     case "MIT":
-      return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)(https://opensource.org/licenses/MIT)"
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
     case "Perl":
-      return "![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)(https://opensource.org/licenses/Artistic-2.0)"
+      return "[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)"
     case "Apache": 
-      return "![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)(https://opensource.org/licenses/Apache-2.0)"
+      return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
     case "IBM":
-      return "![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)(https://opensource.org/licenses/IPL-1.0)"
+      return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"
     case "Mozilla":
-      return "![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)(https://opensource.org/licenses/MPL-2.0)"
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
     default:
       return ""
   }
